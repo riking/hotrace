fe2caa73022bcf3a9ce17dac8c01676933311fb0
Kane York <kyork@student.42.us.org>
2017-05-13 15:04:56 -0700

    OPT: add write buffering
    brings test06 from 9.68 to 9.02

--- src/lib/ft_putstr.c
+++ src/lib/ft_putstr.c
@@ -6,19 +6,35 @@
 /*   By: kyork <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 11:39:50 by kyork             #+#    #+#             */
-/*   Updated: 2017/05/13 14:09:12 by kyork            ###   ########.fr       */
+/*   Updated: 2017/05/13 15:02:27 by kyork            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib.h"
 #include <unistd.h>
 
+static char		g_buf[256];
+static int		g_used_buf;
+
 void	ft_putstr(char const *str)
 {
 	size_t	len;
 
 	len = 0;
-	while (str[len++])
-		;
-	write(1, str, len - 1);
+	while (str[len])
+	{
+		if (g_used_buf == 256)
+		{
+			write(1, g_buf, 256);
+			g_used_buf = 0;
+		}
+		g_buf[g_used_buf++] = str[len++];
+	}
+}
+
+void	flush_stdout(void)
+{
+	if (g_used_buf)
+		write(1, g_buf, g_used_buf);
+	g_used_buf = 0;
 }
--- src/lib/lib.h
+++ src/lib/lib.h
@@ -6,7 +6,7 @@
 /*   By: kyork <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/13 10:17:22 by kyork             #+#    #+#             */
-/*   Updated: 2017/05/13 14:09:03 by kyork            ###   ########.fr       */
+/*   Updated: 2017/05/13 15:02:44 by kyork            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 void		*ft_memalloc(size_t size);
 void		ft_bzero(void *ptr, size_t size);
 void		ft_putstr(const char *str);
+void		flush_stdout(void);
 int			get_next_line0(char **line);
 
 #endif

62be2f76bd7c1d9b3e793c92f8a8c77b6e946ef4
Kane York <kyork@student.42.us.org>
2017-05-14 17:07:33 -0700

    Remove noinline from asm strcmp

--- src/sse/sse_strcmp.c
+++ src/sse/sse_strcmp.c
@@ -6,31 +6,29 @@
 /*   By: kyork <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/13 12:48:16 by kyork             #+#    #+#             */
-/*   Updated: 2017/05/14 16:41:24 by kyork            ###   ########.fr       */
+/*   Updated: 2017/05/14 17:07:06 by kyork            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "str.h"
 
-#define RET int __attribute__((noinline))
-
-RET		sse_strcmp(const char *s1, const char *s2)
+int			sse_strcmp(const char *s1, const char *s2)
 {
 	int		result;
 	size_t	idx;
 
 	__asm__("mov $-16, %1\n"
-			"cmploop: add $16, %1\n"
+			"Lloop%=: add $16, %1\n"
 			"movdqu (%1,%2), %%xmm1\n"
 			"movdqu (%1,%3), %%xmm0\n"
 			"pcmpistri $24, %%xmm0, %%xmm1\n"
-			"jnbe cmploop\n"
-			"jnc equal\n"
+			"jnbe Lloop%=\n"
+			"jnc Lequal%=\n"
 			"add %%rcx, %1\n"
 			"mov $1, %0\n"
-			"jmp done\n"
-			"equal: xor %0, %0\n"
-			"done:nop"
+			"jmp Ldone%=\n"
+			"Lequal%=: xor %0, %0\n"
+			"Ldone%=:nop"
 			: "=r"(result), "+r"(idx)
 			: "r"(s1), "r"(s2)
 			: "rcx", "cc", "xmm0", "xmm1");
